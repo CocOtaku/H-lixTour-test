518192c8c0ce46a7dddbe252ed1303ff32b46449
BucheronV <dhapvirgil1401@gmail.com>
2024-10-15 13:43:23 +0200

    premiere phase

---
+++ View/HomeView.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include('../_partial/_head.php') ?>
+</head>
+<body>
+    <?php
+        include('../_partial/_header.php');
+        include('../_partial/_main.php')
+    ?>
+</body>
+</html>
---
+++ _partial/_header.php
@@ -0,0 +1,6 @@
+<header>
+        <div id="Logo"></div>
+        <div id="Title">
+            <h1>HélixTour</h1>
+        </div>
+</header>
